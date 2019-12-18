8a75dc8049a54ba44bae8fcb6fbfe993e4c63386
Djibril SY <dsy@e2r9p17.42.fr>
2019-12-18 02:32:55 +0100

    + adding author through argument

--- 42_author_checker.sh
+++ 42_author_checker.sh
@@ -37,7 +37,7 @@ then
 	echo "author file found ! this is $GRN$author$NC's project. Let's check files :"
 	LINE1=`grep -R ":..:[0-9][0-9] by [a-z]\|By: [a-z]" ${PRJ}* | wc -l`
 	LINE2=`grep -R ":..:[0-9][0-9] by ${author}\|By: ${author}" ${PRJ}* | wc -l`
-	grep -R ":..:[0-9][0-9] by [a-z]..[a-z]\|By: [a-z][a-z]" ${PRJ}* > ${PRJ}$LOG
+	grep -R ":..:[0-9][0-9] by [a-z]..[a-z]\|By: [a-z][a-z]" ${PRJ}* > $LOG
 	if [ $LINE1 != $LINE2 ];
 	then
 		echo "${RED}FAIL${NC}, some files aren't yours. You can find grep results in the ${YLW}logfile${NC}."
