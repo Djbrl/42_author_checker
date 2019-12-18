07b9513d4555bb9045e51fb9899213cd78056502
Djibril SY <dsy@e2r9p17.42.fr>
2019-12-18 02:50:22 +0100

    + adding author through argument

--- 42_author_checker.sh
+++ 42_author_checker.sh
@@ -19,6 +19,7 @@ echo "${GRN}42 AUTHOR CHECKER${NC}\n"
 if [ -n "$1" ]
 then
 	author=$1
+	echo "You've set the author as $GRN$author$NC. Let's check files :"
 fi
 
 if [ -f "$FILE" ] || [ -n "$1" ];
@@ -31,15 +32,21 @@ then
 	if [ -f "$FILE" ]
 	then
 		author=`cat $FILE`
+		echo "author found ! this is $GRN$author$NC's project. Let's check files :"
 	fi
 	
-	echo "author found ! this is $GRN$author$NC's project. Let's check files :"
 	LINE1=`grep -R ":..:[0-9][0-9] by [a-z]\|By: [a-z]" ${PRJ}* | wc -l`
 	LINE2=`grep -R ":..:[0-9][0-9] by ${author}\|By: ${author}" ${PRJ}* | wc -l`
 	grep -R ":..:[0-9][0-9] by [a-z]..[a-z]\|By: [a-z][a-z]" ${PRJ}* > $LOG
 	if [ $LINE1 != $LINE2 ];
 	then
 		echo "${RED}FAIL${NC}, some files aren't yours. You can find grep results in the ${YLW}logfile${NC}."
+		if [ -f $FILE ]
+		then
+			echo "It is possible that you made a mistake in your ${YLW}author file${NC}."
+		else
+			echo "It is possible that you made a mistake in your ${YLW}argument${NC}."
+		fi
 	else
 		echo "${GRN}SUCCESS!$NC All files are yours, ${GRN}$author$NC."
 		echo "If you don't trust this result, you can find grep results with ${YLW}PATHS$NC in the ${YLW}logfile${NC}."
